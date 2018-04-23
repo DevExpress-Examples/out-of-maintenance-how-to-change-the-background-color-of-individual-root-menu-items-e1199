2e298e9039c9e91a07716c2ad2d41deb0d138622
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to change the background color of individual root menu items
+
+
+<p>This example demonstrates how to define different background colors for root menu items. </p><p>To do this, we use root items <strong>TextTemplate</strong> and place a new table (with a defined background color) inside it. The <strong>ASPxLabel's</strong> Text property is data bound to the menu item's Text property. Note that the control's paddings and spacings should be set to "0".</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,23 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxEditors
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+
+	Protected Sub ASPxMenu1_Load(ByVal sender As Object, ByVal e As EventArgs)
+	 '   ASPxLabel MyLabel = ASPxMenu1.Items[0].FindControl("ASPxLabel1") as ASPxLabel;
+	  '  MyLabel.Text = "Text";
+	End Sub
+	Protected Sub ASPxMenu1_ItemClick(ByVal source As Object, ByVal e As DevExpress.Web.ASPxMenu.MenuItemEventArgs)
+
+	End Sub
+End Class
